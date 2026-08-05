8e5585b348e8532a959f00c3fcb8344648480f1c
Darby Small <darby.c.small@gmail.com>
2026-08-05 11:11:30 -0400

    Set up Flask app and create index route

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask, render_template
 from crud import get_all_tickets
 
-return render_template("index.html")
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    Display all support tickets.
+    """
+
+    tickets = get_all_tickets()
+
+    return render_template(
+        "index.html",
+        tickets=tickets
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
